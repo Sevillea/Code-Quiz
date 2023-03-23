2d6527259e7a0e4a87e2e47ecfc5d24c340f6532
sevillea <aseville92@gmail.com>
2023-03-23 21:35:44 +0000

    answers

--- assets/css/script.js
+++ assets/css/script.js
@@ -64,29 +64,40 @@ const qAndA = [
         A: "Ham, Tomato Mustard, Leek?",
         B: "How do You like My Lasagna?",
         C: "Hyper Text Markup Language",
-        D: "Hi Tom, My Lady"
+        D: "Hi Tom, My Lady",
     },
     {
         q: "What does CSS do?",
         A: "Adds style my HTML",
         B: "Adds functionality to my web page",
         C: "Creates a server for my web page",
-        D: "It's a code used to signal the help of other coders in the area"
+        D: "It's a code used to signal the help of other coders in the area",
     },
     {
         q: "What is Bootstrap?",
         A: "The things I pull myself up by",
         B: "A JavaScript framework",
         C: "A HTML library",
-        D: "A CSS library"
+        D: "A CSS library",
     },
     {
         q: "What command do I need to run my React app?",
         A: "NPM build",
         B: "NPM Start",
         C: "NPM Install",
-        D: "NPM Run Build"
+        D: "NPM Run Build",
     },
 
 ];
+
+// Answers:
+
+const answerKey = [
+    { answer: "C" },
+    { answer: "A" },
+    { answer: "C" },
+    { answer: "B"},
+];
+
+
 
