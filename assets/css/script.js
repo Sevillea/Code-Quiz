c3138ab82711a93455823170981c4964fb79d905
sevillea <aseville92@gmail.com>
2023-03-23 16:38:35 +0000

    setTimer-count

--- assets/css/script.js
+++ assets/css/script.js
@@ -1,2 +1,22 @@
 const timerEl = document.getElementById("timer");
 const timerCard = documentGetElementById("timer-card");
+
+function setTimer() {
+    secondsLeft = 60;
+    timerInterval = setInterval(function(){
+        secondsLeft--;
+        timerEl.textContent.content = secondsLeft;
+        if (secondsLeft === 0 || secondsLeft > 21) {
+            timerEl.style.color = "black";
+        }
+        if (secondsLeft > 10 && secondsLeft < 21) {
+            timerEl.style.color = "yellow";
+        }
+        if (secondsLeft < 10) {
+            timerEl.style.color = "red";
+        }
+        if (secondsLeft<= 0){
+            gameOver();
+        }
+    })
+}
